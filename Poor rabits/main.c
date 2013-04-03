0344b23f3c1e62a8345738476ab186336b98e410
AlexPMenshikov <Skimn1990@gmail.com>
2013-04-03 20:44:29 +0600

    Update main.c

--- Poor rabits/main.c
+++ Poor rabits/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-
+#include <math.h>
 int main()
 {
     int rabits;
+    
     printf(&d,"Hello world!",rabits," \n");
     getch();
     return 0;
